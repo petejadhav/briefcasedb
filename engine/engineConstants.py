4322bbc2c89fb2c76aef07357c03d924d2327967
Prithviraj 'Pete' Jadhav <prithviraj.en@gmail.com>
2018-05-16 19:05:25 +0530

    Update engineConstants.py

--- engine/engineConstants.py
+++ engine/engineConstants.py
@@ -5,3 +5,8 @@ data_directory_name = 'data'
 ENGINEDIR = os.path.dirname(os.path.abspath(__file__))
 ROOTDIR = os.sep.join(ENGINEDIR.split(os.sep)[:-1])
 DATADIR = ROOTDIR + os.sep + data_directory_name
+
+# Changine the following values might result in backward-compatibility issues
+# You have been warned
+MAX_COLUMN_KEY_LENGTH = 10
+
