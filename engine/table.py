39d9265d7d0ea0e9c49e25d649573228ed66e0a6
Prithviraj 'Pete' Jadhav <prithviraj.en@gmail.com>
2018-05-16 18:59:00 +0530

    Update table.py

--- engine/table.py
+++ engine/table.py
@@ -13,7 +13,14 @@ class DBTable:
 		if(!self.database.getIfExists()):
 			print("Error: Database "+ db_name +" not found")
 			return False
+		else:
+			return True
+	
+	def createTable(self):
 		# Create table directory
+		if self.name in os.listdir(ec.DATADIR + os.sep + self.database.name):
+			print("Table already exists")
+			return False
 		self.absTablePath = ec.DATADIR + os.sep + self.database.name + os.sep + self.name
 		os.mkdir(self.absTablePath)
 		return True
@@ -29,7 +36,8 @@ class DBTable:
 		# Validate Json	and create column bin files	
 		for col in columnsObj:
 			c = open(self.absTablePath+os.sep+col['name'],'wb')
-			# write headers
+			# calculate byte_multiplication_factor
+			# write headers to column file
 			c.close()
 		# write schema to config file binary
 		cfgFile.write(json.dumps(jsonStr))
